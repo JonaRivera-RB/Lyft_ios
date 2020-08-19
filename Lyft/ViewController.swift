782dee89efe7e59127030ffd8171f374bbd0759b
Jonathan Rivera <misael@MacBook-Air-de-Misael-5.local>
2020-08-18 23:30:42 -0500

    add base project

---
+++ Lyft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lyft
+//
+//  Created by Misael Rivera on 18/08/20.
+//  Copyright © 2020 Misael Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
